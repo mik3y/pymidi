9ec914d8bf3d99ada10754abd4d7f5d008dfc66d
mike wakerly <opensource@hoho.com>
2018-09-15 16:56:40 -0400

    MIDI command packet processing!
    * Supports running status commands.
    * Journals are partially supported (read but not parsed).

--- pymidi/protocol.py
+++ pymidi/protocol.py
@@ -9,8 +9,9 @@ import random
 import time
 
 from construct import Struct, Const, PaddedString, CString, Padding, Int8ub, Int16ub, Int32ub
-from construct import Int64ub, Bitwise, BitStruct, BitsInteger, Nibble, Flag, Optional
-from construct import If, IfThenElse, GreedyBytes, GreedyRange, VarInt
+from construct import Int64ub, Bitwise, BitStruct, BitsInteger, Nibble, Flag, Optional, Bytes
+from construct import If, IfThenElse, GreedyBytes, GreedyRange, VarInt, FixedSized, Byte, Computed
+from construct import Switch, Enum, Peek
 from construct import this as _this
 
 logger = logging.getLogger('pymidi.protocol')
@@ -52,6 +53,22 @@ MIDIPacketHeader = Struct(
     'ssrc' / Int32ub,
 )
 
+COMMAND_NOTE_OFF = 0x80
+COMMAND_NOTE_ON = 0x90
+COMMAND_AFTERTOUCH = 0xA0
+COMMAND_CONTROL_MODE_CHANGE = 0xB0
+
+
+# Global that stores the last-seen status, for running status support.
+# TODO(mikey): This *should* be possible without any state, by peeking
+# at the struct under construction; haven't figured it out.
+last_command = None
+
+
+def remember_last(obj, ctx):
+    global last_command
+    last_command = obj
+
 
 MIDIPacketCommand = Struct(
     'flags' / BitStruct(
@@ -62,15 +79,82 @@ MIDIPacketCommand = Struct(
         'len' / IfThenElse(_this.b == 0, BitsInteger(4), BitsInteger(12)),
     ),
     # 'midi_list' / Bytes(_this.flags.len),
-    'midi_list' / GreedyRange(Struct(
-        'delta_time' / If(_this._index != 0 or not _this.flags.z, VarInt),
-        'command' / BitStruct(
-            'command' / Nibble,
-            'channel' / Nibble,
+    'midi_list' / FixedSized(_this.flags.len, GreedyRange(Struct(
+        'delta_time' / If(_this._index > 0, VarInt),
+
+        # The "running status" technique means multiple commands may be sent under
+        # the same status. This condition occurs when, after parsing the current
+        # commands, we see the next byte is NOT a status byte (MSB is low).
+        '__next' / Peek(Int8ub),
+        'command_byte' / IfThenElse(_this.__next & 0x80,
+            Byte * remember_last,
+            Computed(lambda ctx: last_command)
         ),
-        'param1' / Int8ub,
-        'param2' / Int8ub,
+        'command' / If(_this.command_byte, Enum(Computed(_this.command_byte & 0xf0),
+            note_on=COMMAND_NOTE_ON,
+            note_off=COMMAND_NOTE_OFF,
+            aftertouch=COMMAND_AFTERTOUCH,
+            control_mode_change=COMMAND_CONTROL_MODE_CHANGE)),
+        'channel' / If(_this.command_byte, Computed(_this.command_byte & 0x0f)),
+
+        'params' / Switch(_this.command, {
+            'note_on': Struct(
+                'key' / Byte,
+                'velocity' / Byte,
+            ),
+            'note_off': Struct(
+                'key' / Byte,
+                'velocity' / Byte,
+            ),
+            'aftertouch': Struct(
+                'key' / Byte,
+                'touch' / Byte,
+            ),
+            'control_mode_change': Struct(
+                'controller' / Byte,
+                'value' / Byte,
+            ),
+        }, default=Struct(
+            'unknown' / GreedyBytes,
+        ))),
     )),
+)
+
+MIDISystemJournal = Struct(
+    'header' / BitStruct(
+        's' / Flag,
+        'd' / Flag,
+        'v' / Flag,
+        'q' / Flag,
+        'f' / Flag,
+        'x' / Flag,
+        'length' / BitsInteger(10),
+    ),
+
+    # Note from RFC 6295 appendix A1: The "length" field includes
+    # the header bytes.
+    'journal' / Bytes(_this.header.length - 2),
+)
+
+MIDIChapterJournal = Struct(
+    'header' / BitStruct(
+        's' / Flag,
+        'chan' / BitsInteger(4),
+        'h' / Flag,
+        'length' / BitsInteger(10),
+        'p' / Flag,
+        'c' / Flag,
+        'm' / Flag,
+        'w' / Flag,
+        'n' / Flag,
+        'e' / Flag,
+        't' / Flag,
+        'a' / Flag,
+    ),
+
+    # Note from RFC 6295 appendix A1: The "length" field includes
+    # the header bytes.
+    'journal' / Bytes(_this.header.length - 3),
 )
 
 MIDIPacketJournal = Struct(
@@ -80,15 +164,16 @@ MIDIPacketJournal = Struct(
         'a' / Flag,
         'h' / Flag,
         'totchan' / BitsInteger(4),
-        'checkpoint_seqnum' / BitsInteger(12),
     ),
-    'remain' / GreedyBytes,
+    'checkpoint_seqnum' / Int16ub,
+    'system_journal' / If(_this.header.s, MIDISystemJournal),
+    'channel_journal' / If(_this.header.a, MIDIChapterJournal),
 )
 
 MIDIPacket = Struct(
     'header' / MIDIPacketHeader,
     'command' / MIDIPacketCommand,
-    'journal' / Optional(MIDIPacketJournal),
+    'journal' / If(_this.command.flags.j, MIDIPacketJournal),
 )
 
 
--- pymidi/tests/protocol_test.py
+++ pymidi/tests/protocol_test.py
@@ -5,17 +5,25 @@ from __future__ import unicode_literals
 
 from unittest import TestCase
 from pymidi import protocol
-
-
-def h2b(s):
-    if hasattr(s, 'decode'):
-        return s.decode('hex')
-    return bytes.fromhex(s)
-
+from pymidi.utils import h2b
 
 EXCHANGE_PACKET = h2b('ffff494e000000026633487347d810966d626f6f6b2d73657373696f6e00')
 TIMESTAMP_PACKET = h2b('ffff434b47d8109602000000000000004400227e00000dfaad1e5c820000000044002288')
-MIDI_PACKET = h2b('806142723be6933947d8109646803b000a39002042550011080567b52db940bb4bbc43be3109a0')
+
+# A midi packet with a Note On command
+SINGLE_MIDI_PACKET = h2b('8061427a4b9f303647d8109643903026204276000608006685')
+
+# A midi packet with two commands, utilixing running status.
+#
+# Commands
+#   0x90 0x3e 0x41  - NOTE_ON D3 velocity 49
+#   0x40 0x3b       - NOTE_ON E3 velocity 59
+#
+# Journal
+#   NOTE_ON C3 velocity 37
+MULTI_MIDI_PACKET = h2b(
+    '8061429a51d2dc8747d8109646903e310a403b21427c00090881673c250d50c8060880440e'
+)
 
 
 class TestPackets(TestCase):
@@ -38,8 +46,9 @@ class TestPackets(TestCase):
         self.assertEqual(15370297433218, pkt.timestamp_2)
         self.assertEqual(1140859528, pkt.timestamp_3)
 
-    def test_midi_packet(self):
-        pkt = protocol.MIDIPacket.parse(MIDI_PACKET)
+    def test_single_midi_packet(self):
+        pkt = protocol.MIDIPacket.parse(SINGLE_MIDI_PACKET)
+        print(pkt)
         self.assert_(pkt.header, 'Expected a header')
         self.assertEqual(2, pkt.header.rtp_header.flags.v)
         self.assertEqual(False, pkt.header.rtp_header.flags.p)
@@ -47,7 +56,59 @@ class TestPackets(TestCase):
         self.assertEqual(0, pkt.header.rtp_header.flags.cc)
         self.assertEqual(False, pkt.header.rtp_header.flags.m)
         self.assertEqual(0x61, pkt.header.rtp_header.flags.pt)
-        self.assertEqual(17010, pkt.header.rtp_header.sequence_number)
+        self.assertEqual(17018, pkt.header.rtp_header.sequence_number)
 
         self.assert_(pkt.command, 'Expected a command')
+        self.assertEqual(False, pkt.command.flags.b)
+        self.assertEqual(True, pkt.command.flags.j)
+        self.assertEqual(False, pkt.command.flags.z)
+        self.assertEqual(False, pkt.command.flags.p)
+        self.assertEqual(3, pkt.command.flags.len)
+        print(pkt.command)
+        self.assertEqual(1, len(pkt.command.midi_list))
+
+        command = pkt.command.midi_list[0]
+        self.assertEqual(0x90, command.command_byte)
+        self.assertEqual('note_on', command.command)
+        self.assertEqual(48, command.params.key)
+        self.assert_(38, command.params.velocity)
+
+        self.assert_(pkt.journal, 'Expected journal')
+
+    def test_multi_midi_packet(self):
+        pkt = protocol.MIDIPacket.parse(MULTI_MIDI_PACKET)
+        self.assert_(pkt.header, 'Expected a header')
+        self.assertEqual(2, pkt.header.rtp_header.flags.v)
+        self.assertEqual(False, pkt.header.rtp_header.flags.p)
+        self.assertEqual(False, pkt.header.rtp_header.flags.x)
+        self.assertEqual(0, pkt.header.rtp_header.flags.cc)
+        self.assertEqual(False, pkt.header.rtp_header.flags.m)
+        self.assertEqual(0x61, pkt.header.rtp_header.flags.pt)
+        self.assertEqual(17050, pkt.header.rtp_header.sequence_number)
+
+        self.assert_(pkt.command, 'Expected a command')
+        self.assertEqual(False, pkt.command.flags.b)
+        self.assertEqual(True, pkt.command.flags.j)
+        self.assertEqual(False, pkt.command.flags.z)
+        self.assertEqual(False, pkt.command.flags.p)
+        self.assertEqual(6, pkt.command.flags.len)
+        self.assertEqual(2, len(pkt.command.midi_list))
+
+        command = pkt.command.midi_list[0]
+        self.assertEqual(0x90, command.command_byte)
+        self.assertEqual('note_on', command.command)
+        self.assertEqual(0x3e, command.params.key)
+        self.assert_(38, command.params.velocity)
+
+        command = pkt.command.midi_list[1]
+        self.assertEqual(0x90, command.command_byte)
+        self.assertEqual('note_on', command.command)
+        self.assertEqual(0x40, command.params.key)
+        self.assert_(38, command.params.velocity)
+
+        self.assert_(pkt.journal, 'Expected journal')
+
+    def test_packet_with_no_journal(self):
+        pkt = protocol.MIDIPacket.parse(h2b('806142a0550d8a5a47d8109603903446'))
+        self.assertEqual(False, pkt.command.flags.j, 'Expected J bit to be clear')
         self.assert_(not pkt.journal, 'Expected no journal')
