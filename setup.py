be995925ec11b1600cb5c4d4de798f0ed7c8f170
mike wakerly <opensource@hoho.com>
2018-09-16 19:26:36 -0400

    Add license.

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='pymidi',
     version='0.1.0',
+    license='MIT',
     url='https://github.com/mik3y/pymidi',
     author='mike wakerly',
     author_email='opensource@hoho.com',
